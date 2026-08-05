32cdb71d07e1e88f067ee08d648af25547276fe9
Divine Lubiku <dilubjhb025@student.wethinkcode.co.za>
2026-08-05 13:36:43 +0200

    feature: New methods - Argument(),OptionType(), Command()

---
+++ command_runner/lib/src/arguments.dart
@@ -0,0 +1,67 @@
+import '../command_runner.dart';
+
+enum OptionType { flag, option }
+
+abstract class Argument {
+  String get name;
+  String? get help;
+
+  Object? get defaultValue;
+  String? get valueHelp;
+
+  String get usuage;
+}
+
+class Option extends Argument {
+  Option(
+    this.name, {
+    required this.type,
+    this.help,
+    this.abbr,
+    this.defaultValue,
+    this.valueHelp,
+  });
+
+  @override
+  final String name;
+  final OptionType type;
+
+  @override
+  final String? help;
+  final String? abbr;
+
+  @override
+  final Object? defaultValue;
+
+  @override
+  final String? valueHelp;
+
+  @override
+  String get usuage {
+    if (abbr != null) {
+      return '-$abbr, --$name: $help';
+    }
+
+    return '--name$name: $help';
+  }
+}
+
+abstract class Command extends Argument {
+  @override
+  String get name;
+
+  String get description;
+
+  bool get requiresArgument => false;
+
+  late CommandRunner runner;
+
+  @override
+  String? help;
+
+  @override
+  String? defaultValue;
+
+  @override
+  String? valueHelp;
+}

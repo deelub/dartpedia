554d394f250976c316ef8e48c30cf1df2f87daee
divine <divinelubiku6@gmail.com>
2026-09-17 22:48:34 +0200

    finished adding cli logger

--- cli/bin/cli.dart
+++ cli/bin/cli.dart
@@ -1,67 +1,34 @@
 import 'dart:io';
 import 'package:http/http.dart' as http;
 import 'package:command_runner/command_runner.dart'; //commandRunner class now available
+import 'package:cli/cli.dart';
+import 'package:command_runner/command_runner.dart';
+
 
 const version = '0.0.1';
-
 void main(List<String> arguments) async {
-  var commandRunner = CommandRunner(
+  final errorLogger = initFileLogger('errors');
+  final app =
+      CommandRunner(
+          onOutput: (String output) async {
+            await write(output);
+          },
+          onError: (Object error) {
+            if (error is Error) {
+              errorLogger.severe(
+                '[Error] ${error.toString()}\n${error.stackTrace}',
+              );
+              throw error;
+            }
+            if (error is Exception) {
+              errorLogger.warning(error);
+              print(error);
+            }
+          },
+        )
+        ..addCommand(HelpCommand())
+        ..addCommand(SearchCommand(logger: errorLogger))
+        ..addCommand(GetArticleCommand(logger: errorLogger));
 
-    onOutput: (String output) async {
-      await write (output);
-    },
-    onError: (Object error) {
-      if (error is Error) {
-        throw error;
-      }
-      if (error is Exception) {
-        print(error);
-      }
-    },
-  )..addCommand(HelpCommand());
-  commandRunner.run(arguments);
-}
-
-Future<void> searchWikipedia(List<String>? arguments) async {
-  final String articleTitle;
-
-  if (arguments == null || arguments.isEmpty) {
-    print('Please provide an article title');
-
-    final inputFromStdin = stdin.readLineSync();
-
-    if (inputFromStdin == null || inputFromStdin.isEmpty) {
-      print('No article title provided. Exiting');
-      return; //Handles no inplut to return nothing
-    }
-    articleTitle = inputFromStdin;
-  } else {
-    articleTitle = arguments.join('');
-  }
-  print('Looking up articles about "$articleTitle". Please wait.');
-
-  var articleContent = await getWikipediaArticle(
-    articleTitle,
-  ); //Calling teh APi and waiting for the outputted results
-  print(articleContent);
-}
-
-void printUsuage() {
-  print(
-    "The following commands are valid: 'help', 'version', 'search <ARTICLE-TITLE>'",
-  );
-}
-
-Future<String> getWikipediaArticle(String articleTitle) async {
-  final url = Uri.https(
-    'en.wikipedia.org',
-    '/api/rest_v1/page/summary/$articleTitle',
-  );
-
-  final response = await http.get(url);
-
-  if (response.statusCode == 200) {
-    return response.body;
-  }
-  return 'Error: Failed to fetch article "$articleTitle". Status codde: ${response.statusCode}';
+  app.run(arguments);
 }

9d0732504e6f83bcc6ddc66c8635c6b47cb85d4c
divine <divinelubiku6@gmail.com>
2026-09-17 13:15:31 +0200

    feat : add renderCommand function that will be used in FutureOr

--- command_runner/lib/src/help_command.dart
+++ command_runner/lib/src/help_command.dart
@@ -1,5 +1,8 @@
 import 'dart:async';
 import 'arguments.dart';
+import 'console.dart';
+import 'exceptions.dart';
+import 'package:command_runner/command_runner.dart';
 
 class HelpCommand extends Command {
   HelpCommand() {
@@ -14,7 +17,13 @@ class HelpCommand extends Command {
       help:
           "When a command is passed as an argument, prints only that command's verbose usage.",
     );
+
   }
+
+
+
+
+  
   @override
   String get name => 'help';
 
@@ -24,13 +33,40 @@ class HelpCommand extends Command {
   @override
   String? get help => 'Prints this usage information';
 
-  @override
-  FutureOr<Object?> run(ArgResults args) async {
-    var usage = runner.usage;
-    for (var command in runner.commands) {
-      usage += '\n ${command.usuage}';
+ @override
+FutureOr<String> run(ArgResults args) async {
+  final buffer = StringBuffer();
+  buffer.writeln(runner.usage.titleText);
+
+  if (args.flag('verbose')) {
+    for (var cmd in runner.commands) {
+      buffer.write(_renderCommandVerbose(cmd));
     }
 
-    return usage;
+    return buffer.toString();
   }
+
+  if (args.hasOption('command')) {
+    var (:option, :input) = args.getOption('command');
+
+    var cmd = runner.commands.firstWhere(
+      (command) => command.name == input,
+      orElse: () {
+        throw ArgumentException(
+          'Input $input is not a known command.',
+          name,
+        );
+      },
+    );
+
+    return _renderCommandVerbose(cmd);
+  }
+
+  // Verbose is false and no arg was passed in, so print basic usage.
+  for (var command in runner.commands) {
+    buffer.writeln(command.usage);
+  }
+
+  return buffer.toString();
+}
 }

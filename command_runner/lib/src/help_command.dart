ba7b532179529bf4268d34bc9c832922c4940bb3
divine <divinelubiku6@gmail.com>
2026-09-17 13:20:00 +0200

    feat : update onOutput to print to output

--- command_runner/lib/src/help_command.dart
+++ command_runner/lib/src/help_command.dart
@@ -20,6 +20,22 @@ class HelpCommand extends Command {
 
   }
 
+  String _renderCommandVerbose(Command cmd) {
+  final indent = ' ' * 10;
+  final buffer = StringBuffer();
+  buffer.writeln(cmd.usage.instructionText); //abbr, name: description
+  buffer.writeln('$indent ${cmd.help}');
+  if (cmd.valueHelp != null) {
+    buffer.writeln(
+      '$indent [Argument] Required? ${cmd.requiresArgument}, Type: ${cmd.valueHelp}, Default: ${cmd.defaultValue ?? 'none'}',
+    );
+  }
+  buffer.writeln('$indent Options:');
+  for (var option in cmd.options) {
+    buffer.writeln('$indent ${option.usage}');
+  }
+  return buffer.toString();
+}
 
 
 
